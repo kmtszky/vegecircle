67feeb3aa5f518888e71e68090d6008ab6483f2c
kmtszky <kmtszk711+IT@gmail.com>
2021-05-08 13:54:28 +0000

    [Add] gems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require jquery3
+//= require jquery_ujs
+//= require popper
+//= require bootstrap-sprockets
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
