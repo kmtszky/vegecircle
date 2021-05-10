4918fb73e9a51601850453856f18cdc5cc4a1585
kmtszky <kmtszk711+IT@gmail.com>
2021-05-10 17:22:09 +0000

    [Fix] RecipeTags table => 外部キーをbigint型へ修正

--- db/migrate/20210510165017_change_column_to_recipe_tags.rb
+++ db/migrate/20210510165017_change_column_to_recipe_tags.rb
@@ -1,12 +1,12 @@
 class ChangeColumnToRecipeTags < ActiveRecord::Migration[5.2]
 
   def up
-    change_column :recipe_tags, :recipe_id, :integer
-    change_column :recipe_tags, :tag_id, :integer
+    change_column :recipe_tags, :recipe_id, :bigint
+    change_column :recipe_tags, :tag_id, :bigint
   end
 
   def down
-    change_column :recipe_tags, :recipe_id, :bigint
-    change_column :recipe_tags, :tag_id, :bigint
+    change_column :recipe_tags, :recipe_id, :integer
+    change_column :recipe_tags, :tag_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_165017) do
+ActiveRecord::Schema.define(version: 2021_05_09_162228) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -140,7 +140,7 @@ ActiveRecord::Schema.define(version: 2021_05_10_165017) do
 
   create_table "recipe_tags", force: :cascade do |t|
     t.integer "recipe_id", null: false
-    t.integer "tag_id", null: false
+    t.bigint "tag_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["recipe_id", "tag_id"], name: "index_recipe_tags_on_recipe_id_and_tag_id", unique: true
