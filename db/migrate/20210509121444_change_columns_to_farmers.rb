93d79a10036be577f13d4418523ee8017437b850
kmtszky <kmtszk711+IT@gmail.com>
2021-05-10 18:28:50 +0000

    [Fix] RecipeTags table => 外部キーをbigint型へ修正 #5

--- db/migrate/20210509121444_change_columns_to_farmers.rb
+++ db/migrate/20210509121444_change_columns_to_farmers.rb
@@ -1,5 +1,5 @@
 class ChangeColumnsToFarmers < ActiveRecord::Migration[5.2]
-  def change
+  def reversible
     change_column :farmers, :name,            :string, null: false
     change_column :farmers, :farmer_image_id, :string
     change_column :farmers, :image_1_id,      :string
