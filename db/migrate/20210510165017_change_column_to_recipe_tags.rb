a142f4b0d402dbba083a717964ac9d870376ea9e
kmtszky <kmtszk711+IT@gmail.com>
2021-05-10 17:42:12 +0000

    [Fix] RecipeTags table => 外部キーをbigint型へ修正 #3

--- db/migrate/20210510165017_change_column_to_recipe_tags.rb
+++ db/migrate/20210510165017_change_column_to_recipe_tags.rb
@@ -2,11 +2,9 @@ class ChangeColumnToRecipeTags < ActiveRecord::Migration[5.2]
 
   def up
     change_column :recipe_tags, :recipe_id, :bigint
-    change_column :recipe_tags, :tag_id, :bigint
   end
 
   def down
     change_column :recipe_tags, :recipe_id, :integer
-    change_column :recipe_tags, :tag_id, :integer
   end
 end
