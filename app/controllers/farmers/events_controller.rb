5067751f975bd75f29b861218b2954861a52e7fd
kmtszky <kmtszky711+IT@gmail.com>
2021-05-14 17:55:03 +0000

    [Fix中] event controller

--- app/controllers/farmers/events_controller.rb
+++ app/controllers/farmers/events_controller.rb
@@ -1,23 +1,24 @@
+require "date"
+
 class Farmers::EventsController < ApplicationController
   before_action :authenticate_farmer!, exept: [:event]
   before_action :set_event, except: [:new, :create]
-
-  require "date"
 
   def new
     @event = Event.new
   end
 
   def create
-    start_d = Data.parse(params[:event][:start_date])
-    end_d = Data.parse(params[:event][:start_date])
-    number_of_days = end_d - start_d
+    @event = Event.new(event_params)
 
-    if ( start_d < Data.today ) || ( end_d < Data.today )
+    if @event.start_date.invalid? || @event.end_date.invalid?
       render :new
     else
+      start_d = Date.parse(params[:event][:start_date])
+      end_d = Date.parse(params[:event][:start_date])
+      number_of_days = end_d - start_d
+
       start_d.step(start_d + number_of_days, 1) do |day|
-        @event = Event.new(event_params)
         @event.day = day
         @event.save
       end
@@ -25,13 +26,13 @@ class Farmers::EventsController < ApplicationController
   end
 
   def edit
-    if @event.date < Data.today
+    if @event.date < Date.today
       redirect_to request.referer, flash: { danger: 'イベント日以降のため編集できません'}
     end
   end
 
   def update
-    unless  @event.date < Data.today
+    unless  @event.date < Date.today
       updated_date = Date.parse(params[:event][:date])
       if @event.update(event_params) || @event.update(date: updated_date)
         redirect_to event_path(@event), flash: { success: '農業体験を更新しました' }
@@ -50,7 +51,7 @@ class Farmers::EventsController < ApplicationController
   end
 
   def destroy
-    if @event.date > Data.today
+    if @event.date > Date.today
       @event.destroy
       redirect_to farmers_recipe_index_path, flash: { success: '農業体験を削除しました'}
     else
@@ -69,6 +70,6 @@ class Farmers::EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:title, :plan_image, :body, :fee, :cancel_change, :start_time, :end_time, :location, :access, :parking, :etc, :is_deleted)
+    params.require(:event).permit(:title, :plan_image, :body, :fee, :cancel_change, :start_time, :end_time, :location, :access, :parking, :etc, :is_deleted, :start_date, :end_date)
   end
 end
