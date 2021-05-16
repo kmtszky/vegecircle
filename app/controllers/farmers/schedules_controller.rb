7f5cb0b96543abca72374712c5cacf04abbe0685
kmtszky <kmtszky711+IT@gmail.com>
2021-05-16 14:37:41 +0000

    [Update] scheduleが消えていたため再度編集中 #1, newsのajax #1

---
+++ app/controllers/farmers/schedules_controller.rb
@@ -0,0 +1,44 @@
+class Customers::SchedulesController < ApplicationController
+  before_action :authenticate_farmer!
+  before_action :set_schedule, only: [:show, :edit, :update, :destroy]
+
+  def create
+    @recipe = current_farmer.recipes.new(recipe_params)
+    @tag_list = params[:recipe][:tag_ids].split(',')
+    if @recipe.save
+      @recipe.save_tags(@tag_list)
+      redirect_to farmers_recipe_path(@recipe), flash: {success: "レシピを作成しました"}
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @tag_list = @recipe.tags.pluck(:tag).join(",")
+  end
+
+  def update
+    tag_list = params[:recipe][:tag_ids].split(',')
+    if @recipe.update_attributes(recipe_params)
+      @recipe.save_tags(tag_list)
+      redirect_to farmers_recipe_path(params[:id]), flash: {success: "レシピを更新しました"}
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to farmer_path(current_farmer), flash: {success: "レシピを削除しました"}
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe_image, :duration, :amount, :ingredient, :recipe, :tag_ids)
+  end
+end
