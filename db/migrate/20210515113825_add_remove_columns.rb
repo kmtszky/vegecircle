b14f7001316d90c57e53bc1c50da2ece0c4cba37
kmtszky <kmtszk711+IT@gmail.com>
2021-05-22 16:41:10 +0000

    [Fix] server : databse load error

--- db/migrate/20210515113825_add_remove_columns.rb
+++ db/migrate/20210515113825_add_remove_columns.rb
@@ -1,8 +1,5 @@
 class AddRemoveColumns < ActiveRecord::Migration[5.2]
   def change
-    add_column :schedules, :is_deleted, :boolean, default: false
-    add_reference :reservations, :schedule, foreign_key: true
-    remove_column :events, :is_deleted, :boolean, default: false
     remove_column :reservations, :event, foreign_key: true
 
     remove_index :reservations, :event_id
