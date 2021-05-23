2689aff5216f4a6638162881ec26ba010e3e53ee
kmtszky <kmtszk711+IT@gmail.com>
2021-05-23 05:51:45 +0000

    [Fix] mysql error解消のため

--- db/migrate/20210515113825_add_remove_columns.rb
+++ db/migrate/20210515113825_add_remove_columns.rb
@@ -1,7 +1,7 @@
 class AddRemoveColumns < ActiveRecord::Migration[5.2]
   def change
     add_column :schedules, :is_deleted, :boolean, default: false
-    add_reference :reservations, :schedule, foreign_key: true
+    add_reference :reservations, :schedule
     remove_column :events, :is_deleted, :boolean, default: false
     remove_column :reservations, :event_id, foreign_key: true
   end
