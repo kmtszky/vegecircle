d341e242cb631e99ace73637f0560257170754fb
kmtszky <kmtszk711+IT@gmail.com>
2021-05-23 05:17:55 +0000

    [Update]

--- db/migrate/20210515113825_add_remove_columns.rb
+++ db/migrate/20210515113825_add_remove_columns.rb
@@ -1,2 +1,9 @@
 class AddRemoveColumns < ActiveRecord::Migration[5.2]
+  def change
+    add_column :schedules, :is_deleted, :boolean, default: false
+    add_reference :reservations, :schedule, foreign_key: true
+    remove_column :events, :is_deleted, :boolean, default: false
+    remove_column :reservations, :event_id, foreign_key: true
+    remove_index :reservations, :event_id
+  end
 end
--- db/migrate/20210515124353_change_schedule_to_null_false.rb
+++ db/migrate/20210515124353_change_schedule_to_null_false.rb
@@ -1,6 +1,9 @@
 class ChangeScheduleToNullFalse < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_column :reservations, :schedule_id, :integer, null: false
-    remove_column :reservations, :event_id, :integer
+  end
+
+  def down
+    change_column :reservations, :schedule_id, :integer, null: true
   end
 end
