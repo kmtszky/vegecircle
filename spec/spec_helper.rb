ac142a2ff32b8eb66b1d0855feec2a8abdb78057
kmtszky <kmtszk711+IT@gmail.com>
2021-06-12 23:43:12 +0900

    [Update] RSpec 結合テスト

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,16 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'capybara/rspec'
 RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    # driven_by :selenium_chrome_headless
+    driven_by :rack_test
+  end
+  # config.after(:suite) do
+  #   DatabaseCleaner.strategy = :transaction
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
